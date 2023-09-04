2c3488e95f5c6c0782f50e45131ded8611f34d07
ThiagoMaritns11 <thiago.marques29@etec.sp.gov.br>
2023-09-04 12:40:29 -0300

    adc Controller

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/contato', function () {
+    $message = 'Contato';
+    return ($message) ;
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
